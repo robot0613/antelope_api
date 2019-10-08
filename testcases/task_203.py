8ec22f813f7e39c97bb6cdf69ad2b77a72abda38
sunyan0613 <1310184504@qq.com>
2019-10-08 12:00:03 +0800

    10月8第二次

---
+++ testcases/task_203.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2019/10/8 11:52
+# @Author   : sunyan
+
+import unittest
+import json
+from common.request import Request
+from common.readdata import ReadExcel
+from base.gettoken import get_agv_token
+
+#读取表格中的数据
+test_data = ReadExcel('tasks.xlsx','test_task_203').data_list()
+
+class Task_203(unittest.TestCase):
+
+    def setUp(self):
+
+        self.request = Request()
+        self.token = get_agv_token()
+#
+    def test_task_203(self):
+        global test_data
+        test_data = test_data[0]
+
+        #请求参数
+        para = test_data['para']
+        para = json.loads(para)
+
+        #请求头
+        header = {"Content-Type":"application/x-www-form-urlencoded","token":self.token}
+
+
+        #请求
+        # print('xxx', test_data['url'])
+        # _url = '{}?token={}'.format(test_data['url'], self.token)
+        # r = self.request.request(test_data['method'], _url, para, header)
+        r = self.request.request(test_data['method'], test_data['url'], para, header)
+        actual_result = r.json()
+        # print(actual_result)
+
+        #断言
+        self.assertEqual(actual_result['code'],test_data['code'])
+
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+
+    unittest.main()
+
+
+
